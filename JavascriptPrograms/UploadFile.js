ba8500952f25b0f63c6cc42bdfe0d2f6a99d1006
Sachin Lohani <schnlohani@gmail.com>
2020-02-20 11:47:28 +0530

    File Upload In Js

---
+++ JavascriptPrograms/UploadFile.js
@@ -0,0 +1,27 @@
+// How to upload files to the server using JavaScript
+
+<input type="file" id="fileUpload"></input>;
+
+const handleFileUpload = event => {
+  const files = event.target.files;
+  const formData = new FormData();
+  formData.append("myfiles", files[0]);
+
+  fetch("/saveImage", {
+    method: "POST",
+    body: formData
+  })
+    .then(response => {
+      response.json();
+    })
+    .then(data => {
+      console.log(data.path);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+document.querySelector("#fileUpload").addEventListener("change", event => {
+  handleFileUpload(event);
+});
