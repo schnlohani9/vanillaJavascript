0dedf76fada64857664a1cd0b4bd994a5587566d
Sachin Lohani <slohani@netspend.com>
2021-04-26 23:13:01 +0530

    adding search element

--- JavascriptPrograms/ArrayPrograms/SearchElement.js
+++ JavascriptPrograms/ArrayPrograms/SearchElement.js
@@ -1,2 +1,20 @@
 // what is the optimise way to search element in array using js
-console.log(Object.getPrototypeOf([]))
+
+// var self = this;
+// return self.find(item => 
+//     lookUpArray.every(lookUpItem => 
+//         item[lookUpItem.key] === lookUpItem.val));
+
+function searchElementUsingGenericAlgo(array, value) {
+    let idx = -1;
+    let length = array.length;
+    for (let i = 0;i<length; i++) { /* n+1 comparisons. */
+          if(value == array[i]) { /* n comparisons. */
+               idx = i;
+               break;
+          }
+    }
+    return idx;
+}
+
+console.log(searchElementUsingGenericAlgo([5,3,65,46,345,345,234,5234,234,234],46));
